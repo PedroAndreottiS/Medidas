764e52cb02b2422f39409d85a05a711c7c48166c
PedroAndreottiS <PedroAndreottijetforce@gmail.com>
2023-04-27 17:20:26 -0300

    Finished

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conversor de Milhas para Km, insira as milhas...");
+double milhas, km;
+
+milhas = Convert.ToDouble(Console.ReadLine());
+
+km= milhas * 1.609;
+
+Console.WriteLine($"{km} quilômetros");
